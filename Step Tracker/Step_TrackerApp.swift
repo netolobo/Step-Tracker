b5aafc6902d74d237276f16e63cc03e150f9b082
netolobo <desilio@gmail.com>
2024-06-17 11:51:36 -0300

    Initial Commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Neto Lobo on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
