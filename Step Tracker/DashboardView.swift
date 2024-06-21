cecf7b653074e033fed6d534159c4d5b2d997bfd
Desilio Neto <desilio@gmail.com>
2024-06-21 15:43:40 -0300

    step bar chart interactivity

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -28,11 +28,20 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HelthMetricContext = .steps
+    @State private var rawSelectedDate: Date?
     private var isSteps: Bool { selectedStat == .steps }
     var avgStepCount: Double {
         guard !hkManager.stepData.isEmpty else { return 0}
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
         return totalSteps/Double(hkManager.stepData.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
     }
     
     var body: some View {
@@ -68,6 +77,15 @@ struct DashboardView: View {
                             .foregroundStyle(.secondary)
                             
                             Chart {
+                                if let selectedHealthMetric {
+                                    RuleMark(x: .value("Selected metric", selectedHealthMetric.date, unit: .day))
+                                        .foregroundStyle(.secondary.opacity(0.3))
+                                        .offset(y: -10)
+                                        .annotation(
+                                            position: .top,
+                                            spacing: 0,
+                                            overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotationView }
+                                }
                                 RuleMark(y: .value("Average", avgStepCount))
                                     .foregroundStyle(.secondary)
                                     .lineStyle(.init(lineWidth: 1, dash: [5]))
@@ -78,9 +96,11 @@ struct DashboardView: View {
                                         y: .value("Steps",steps.value)
                                     )
                                     .foregroundStyle(Color.pink.gradient)
+                                    .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1 : 0.3)
                                 }
                             }
                             .frame(height: 150)
+                            .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                             .chartXAxis {
                                 AxisMarks {
                                     AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -101,15 +121,15 @@ struct DashboardView: View {
                     
                     VStack {
                         VStack(alignment: .leading) {
-                                VStack(alignment: .leading) {
-                                    Label("Averages", systemImage: "calendar")
-                                        .font(.title3.bold())
-                                        .foregroundColor(.pink)
-                                    
-                                    Text("Last 28 Days")
-                                        .font(.caption)
-                                        .foregroundStyle(.secondary)
-                                }
+                            VStack(alignment: .leading) {
+                                Label("Averages", systemImage: "calendar")
+                                    .font(.title3.bold())
+                                    .foregroundColor(.pink)
+                                
+                                Text("Last 28 Days")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
                             .padding(.bottom, 12)
                             
                             RoundedRectangle(cornerRadius: 12)
@@ -137,6 +157,25 @@ struct DashboardView: View {
             }
         }
         .tint(isSteps ? .pink : .indigo)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now,
+                 format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+            .font(.footnote.bold())
+            .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color.init(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
     }
 }
 
